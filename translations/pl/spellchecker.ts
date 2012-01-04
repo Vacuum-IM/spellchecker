2722bceffa0a5e6b380668afb674f86f33cc0156
Sergey Potapov <potapov.s.a@gmail.com>
2012-01-04 14:28:48 +0400

    Поправлен скрипт сборки и установки локализаций, который перестал работать в Qt 4.8.

---
+++ translations/pl/spellchecker.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>SpellChecker</name>
+    <message>
+        <source>Spell Checker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Highlights words that may not be spelled correctly</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
